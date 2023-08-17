e2723c280feaa85da342348a7522cb400f5f0ca6
sunquan <sunquan@sway.video>
2023-08-17 20:51:05 +0800

    [Fix]update C++/Hellowworld/opencv_demo

---
+++ computerphotography/C++/helloworld/build/CMakeFiles/opencv_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_demo.dir/opencv_1.cpp.o"
+  "CMakeFiles/opencv_demo.dir/opencv_1.cpp.o.d"
+  "opencv_demo"
+  "opencv_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
